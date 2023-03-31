785e4fdc7e379eb73427df1faf85ed869d8b1a4f
Stéphane Menut <smenut@dawan.fr>
2023-03-31 17:20:50 +0200

    cache http

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    /*
+     * @Cache(public=true, must-revalidate=true, maxage: ... )
+     */
+
     #[Route('/', name: 'home_index')]
+    #[Cache()]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $response = $this->render('home/index.html.twig', []);
+
+        $now = new \DateTime();
+
+        $expire = new \DateTime();
+        $expire->modify('+24 hours');
+        // dump($expire);
+
+        /*$response
+            ->setPublic()
+            ->setDate($now)
+            ->setExpires($expire)
+        ;*/
+
+        $response
+            ->setPublic()
+            ->setSharedMaxAge($expire->getTimestamp());
+
+        return $response;
     }
 }
